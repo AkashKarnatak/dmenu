d2cbbdad274b234da6a5c522f1c9d0c88dabc25f
Akash Karnatak <akashkarnatak.ak47@gmail.com>
2021-07-25 20:46:33 +0530

    Initial commit. Patched dmenu with sonokai theme

---
+++ config.h
@@ -0,0 +1,28 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char col_gray1[]       = "#2c2e34";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#e2e2e3";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#8d7cbf";
+static const char *colors[SchemeLast][3] = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeOut]  = { col_gray4, col_cyan,  col_cyan  },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
