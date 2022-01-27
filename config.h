048d7e3d69c3de72dcbe354bd29b040ca19cc2af
Akash Karnatak <akashkarnatak.ak47@gmail.com>
2022-01-27 23:35:52 +0530

    Updated configs

--- config.h
+++ config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono Nerd Font:style:medium:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char col_gray1[]       = "#2c2e34";
+static const char col_gray1[]       = "#282c34";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#e2e2e3";
 static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#8d7cbf";
+static const char col_cyan[]        = "#b26cc7";
 static const char *colors[SchemeLast][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -21,6 +21,13 @@ static const char *colors[SchemeLast][3] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
+
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                 /* minimum width when centered */
+
+/* Size of the window border */
+static const unsigned int border_width = 0;
 
 /*
  * Characters not considered part of a word while deleting words
